c9be3c1132bbf2f1389aec10b734b688d93241d2
ucarrr <ucaaar07@gmail.com>
2024-08-11 13:51:43 +0300

    initial commit

---
+++ src/main/java/com/patika/bloghubemailservice/BloghubEmailServiceApplication.java
@@ -0,0 +1,13 @@
+package com.patika.bloghubemailservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BloghubEmailServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BloghubEmailServiceApplication.class, args);
+    }
+
+}
